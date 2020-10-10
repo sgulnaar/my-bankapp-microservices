8816affcdbd76f9d5734b84b1baac004b77e0753
sgulnaar <60265822+sgulnaar@users.noreply.github.com>
2020-10-10 03:00:14 -0400

    Add files via upload
    Bank app with microservices

---
+++ my-bankapp-microservices/src/main/java/com/pnc/pnc/PncApplication.java
@@ -0,0 +1,13 @@
+package com.pnc.pnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PncApplication.class, args);
+	}
+
+}
---
+++ my-bankapp-microservices/src/test/java/com/pnc/pnc/PncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnc.pnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
